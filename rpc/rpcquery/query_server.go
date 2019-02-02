425bee878cf1ef8b809845e53f206c0798db5b40
E. Fynn <me@enriquefynn.com>
2019-02-02 13:15:34 +0100

    implementing grpc to get signed headers

--- rpc/rpcquery/query_server.go
+++ rpc/rpcquery/query_server.go
@@ -5,43 +5,44 @@ import (
 	"fmt"
 
 	"github.com/hyperledger/burrow/acm"
-	"github.com/hyperledger/burrow/acm/acmstate"
 	"github.com/hyperledger/burrow/acm/validator"
 	"github.com/hyperledger/burrow/bcm"
 	"github.com/hyperledger/burrow/consensus/tendermint"
+	"github.com/hyperledger/burrow/event"
 	"github.com/hyperledger/burrow/event/query"
+	"github.com/hyperledger/burrow/execution/exec"
 	"github.com/hyperledger/burrow/execution/names"
 	"github.com/hyperledger/burrow/execution/proposal"
 	"github.com/hyperledger/burrow/execution/state"
 	"github.com/hyperledger/burrow/logging"
 	"github.com/hyperledger/burrow/rpc"
+	rpcevents "github.com/hyperledger/burrow/rpc/rpcevents"
 	"github.com/hyperledger/burrow/txs/payload"
 	"github.com/tendermint/tendermint/abci/types"
-	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 type queryServer struct {
-	accounts    acmstate.IterableStatsReader
-	nameReg     names.IterableReader
-	proposalReg proposal.IterableReader
-	blockchain  bcm.BlockchainInfo
-	validators  validator.History
-	nodeView    *tendermint.NodeView
-	logger      *logging.Logger
+	accounts     state.IterableStatsReader
+	nameReg      names.IterableReader
+	proposalReg  proposal.IterableReader
+	blockchain   bcm.BlockchainInfo
+	nodeView     *tendermint.NodeView
+	logger       *logging.Logger
+	subscribable event.Subscribable
 }
 
 var _ QueryServer = &queryServer{}
 
-func NewQueryServer(state acmstate.IterableStatsReader, nameReg names.IterableReader, proposalReg proposal.IterableReader,
-	blockchain bcm.BlockchainInfo, validators validator.History, nodeView *tendermint.NodeView, logger *logging.Logger) *queryServer {
+func NewQueryServer(state state.IterableStatsReader, nameReg names.IterableReader, proposalReg proposal.IterableReader,
+	blockchain bcm.BlockchainInfo, nodeView *tendermint.NodeView, logger *logging.Logger, subscribable event.Subscribable) *queryServer {
 	return &queryServer{
-		accounts:    state,
-		nameReg:     nameReg,
-		proposalReg: proposalReg,
-		blockchain:  blockchain,
-		validators:  validators,
-		nodeView:    nodeView,
-		logger:      logger,
+		accounts:     state,
+		nameReg:      nameReg,
+		proposalReg:  proposalReg,
+		blockchain:   blockchain,
+		nodeView:     nodeView,
+		logger:       logger,
+		subscribable: subscribable,
 	}
 }
 
@@ -167,6 +168,19 @@ func (qs *queryServer) ListProposals(param *ListProposalsParam, stream Query_Lis
 	return streamErr
 }
 
+func (qs *queryServer) ListSignedHeaders(request *rpcevents.BlocksRequest, stream Query_ListSignedHeadersServer) error {
+	return qs.streamSignedHeaders(stream.Context(), request.BlockRange, func(signedHeaders *SignedHeadersResult) error {
+		if signedHeaders != nil {
+			// fmt.Printf("Signed HEADER: %v", signedHeader.Commit)
+			err := stream.SendMsg(signedHeaders)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
 func (qs *queryServer) GetStats(ctx context.Context, param *GetStatsParam) (*Stats, error) {
 	stats := qs.accounts.GetAccountStats()
 
@@ -176,13 +190,78 @@ func (qs *queryServer) GetStats(ctx context.Context, param *GetStatsParam) (*Sta
 	}, nil
 }
 
-// Tendermint and blocks
+func (qs *queryServer) streamSignedHeaders(ctx context.Context, blockRange *rpcevents.BlockRange,
+	consumer func(*SignedHeadersResult) error) error {
 
-func (qs *queryServer) GetBlockHeader(ctx context.Context, param *GetBlockParam) (*types.Header, error) {
-	header, err := qs.blockchain.GetBlockHeader(param.Height)
+	// Converts the bounds to half-open interval needed
+	start, end, streaming := blockRange.Bounds(qs.blockchain.LastBlockHeight())
+	qs.logger.TraceMsg("Streaming signed block headers", "start", start, "end", end, "streaming", streaming)
+
+	// Pull blocks from state and receive the upper bound (exclusive) on the what we were able to send
+	// Set this to start since it will be the start of next streaming batch (if needed)
+	// TODO
+	// start, err := qs.iterateBlocks(start, end, consumer)
+
+	// If we are not streaming and all blocks requested were retrieved from state then we are done
+	// TODO
+	// if !streaming && start == end {
+	// 	return err
+	// }
+
+	// Otherwise we need to begin streaming blocks as they are produced
+	subID := event.GenSubID()
+	// Subscribe to BlockExecution events
+	out, err := qs.subscribable.Subscribe(ctx, subID, exec.QueryForBlockExecutionFromHeight(end),
+		rpcevents.SubscribeBufferSize)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	abciHeader := tmtypes.TM2PB.Header(header)
-	return &abciHeader, nil
+	defer qs.subscribable.UnsubscribeAll(context.Background(), subID)
+
+	for msg := range out {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			block := msg.(*exec.BlockExecution)
+			streamEnd := block.Height
+
+			finished := !streaming && streamEnd >= end
+			if finished {
+				// Truncate streamEnd to final end to get exactly the blocks we want from state
+				streamEnd = end
+			}
+			if start < streamEnd {
+				// This implies there are some blocks between the previous batchEnd (now start) and the current BlockExecution that
+				// we have not emitted so we will pull them from state. This can occur if a block is emitted during/after
+				// the initial streaming but before we have subscribed to block events or if we spill BlockExecutions
+				// when streaming them and need to catch up
+				// TODO
+				// _, err := ees.iterateBlocks(start, streamEnd, consumer)
+				// if err != nil {
+				// 	return err
+				// }
+			}
+			if finished {
+				return nil
+			}
+			commit := qs.nodeView.BlockStore().LoadBlockCommit(int64(block.Height - 1))
+			header := qs.nodeView.BlockStore().LoadBlockMeta(int64(block.Height - 1)).Header
+			signedHeader := types.SignedHeader{
+				Commit: commit,
+				Header: &header,
+			}
+			signedHeaderResult := SignedHeadersResult{
+				SignedHeader: &signedHeader,
+			}
+			err = consumer(&signedHeaderResult)
+			if err != nil {
+				return err
+			}
+			// We've just streamed block so our next start marker is the next block
+			start = block.Height + 1
+		}
+	}
+
+	return nil
 }
